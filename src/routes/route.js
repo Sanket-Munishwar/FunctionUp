037c7ce5b59957398e8455192dbec1d3fe32c688
Sanket Munishwar <sanketmunishwar7@gmail.com>
2023-04-21 20:05:32 +0530

    update the assignment for get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,52 +1,51 @@
 const express = require('express');
-const { route } = require('express/lib/application');
 const router = express.Router();
-const commonFile = require('./common')
-const myUnderscore = require('underscore')
 
-router.get('/test-me', function (req, res) {
-    res.send('This should be working!')
-});
 
-router.get('/test-you', function (req, res) {
-    console.log('The exported module is: ',commonFile)
-    commonFile.doSomething()
-    console.log('This is the constant I created', commonFile.name)
-    res.send('Hello there, welcome to this application!')
-});
 
-router.get('/test-underscore', function(req, res){
-    let result = myUnderscore.first([11,12,23,44,15], 4)
-    console.log('the result is',result)
-    res.send('done')
-})
-
-router.get('/cohorts', function (request, response){
-    // logic to get the cohorts from database
-    // logic tp get only the active cohorts
-    // logic to get only the cohort with a size than 50
-    // logic to get only the backend cohorts
-    response.send(['technetium','nobelium'])
-})
-
-router.get('/students', function(req, res){
-    // receive or access the query params in the code
-    // write a logic on these query params
-    // city, score
+// let movies = ['avengers','fast and furious','iron man','godzilla','the eternity']
+router.get('/movies', function(req,res){
+    let movieCollection = ['avengers','fast and furious','iron man','godzilla','the eternity']
     console.log(req.query)
-    let requestedCity = req.query.city
-    let sortField = req.query.sort
-    // logic to get students
-    res.send(["Sabiha","Neha","Akash","Sonali"])
+    res.send(movies)
 })
 
-router.get('/students/:studentName', function(req, res) {
-    console.log(req.params.studentName)
-    /// go to database and search for studentName student
-    // store the data found in this variable - studentDetails
-    //res.send({data: studentDetails})
-    res.send('student data')
+router.get('/movies/:indexName', function(req, res){
+    let movieCollection = ['avengers','fast and furious','iron man','godzilla','the eternity']
+    if(req.params.indexName > movieCollection.length || req.params.indexName < 0){
+        return res.send('Write a valid index number')
+    }
+    else{
+        for(let i=0; i < movieCollection.length; i++){
+            if(req.params.indexName == i){
+                return res.send(movieCollection[i])
+            }
+        }
+    }
+    
 })
+router.get('/films/:filmId', function (req, res){
+    let ArrObj=[ {
+      'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+    const ans=ArrObj.find(x=> x.id==req.params.filmId)
+       if(ans){
+        res.send(ans)
+       }else{
+        res.send('No movie exists with this id')
+       }
+})
+
 
 
 module.exports = router;
