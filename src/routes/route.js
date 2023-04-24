8b739e8659df3f96fe684c0e382e35fadf63f741
Sanket Munishwar <sanketmunishwar7@gmail.com>
2023-04-24 22:32:51 +0530

    updating missing number assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,52 +1,40 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
-const commonFile = require('./common')
-const myUnderscore = require('underscore')
+
 
 router.get('/test-me', function (req, res) {
     res.send('This should be working!')
 });
 
-router.get('/test-you', function (req, res) {
-    console.log('The exported module is: ',commonFile)
-    commonFile.doSomething()
-    console.log('This is the constant I created', commonFile.name)
-    res.send('Hello there, welcome to this application!')
-});
+router.get('/solu1',function(req, res){
+    let arr = [1,2,3,5,6,7];
+    let missingNumber = 0
+    for (let i=0; i< arr.length; i++){
+        missingNumber += arr[i]  // missingNumber = missingNumber + arr[i]
+    }
+    let n = arr[arr.length - 1]
+    let result = n * (n + 1)/2
+    res.send( { data: result - missingNumber})
 
-router.get('/test-underscore', function(req, res){
-    let result = myUnderscore.first([11,12,23,44,15], 4)
-    console.log('the result is',result)
-    res.send('done')
 })
 
-router.get('/cohorts', function (request, response){
-    // logic to get the cohorts from database
-    // logic tp get only the active cohorts
-    // logic to get only the cohort with a size than 50
-    // logic to get only the backend cohorts
-    response.send(['technetium','nobelium'])
-})
+router.get('/solu2',function(req, res){
+    let arr = [33,34,35,37,38];
+    let n = arr.length
+    let total = 0
+    for (let i in arr){
+        total += arr[i] // total = total + arr[i]
+    }
+    let first = arr[0]
+    let last = arr.pop()
+    
+    let sum = (n+1) * (first + last)/2
+    let missingNumber = sum - total
 
-router.get('/students', function(req, res){
-    // receive or access the query params in the code
-    // write a logic on these query params
-    // city, score
-    console.log(req.query)
-    let requestedCity = req.query.city
-    let sortField = req.query.sort
-    // logic to get students
-    res.send(["Sabiha","Neha","Akash","Sonali"])
-})
+    res.send( { data : missingNumber})
+    
 
-router.get('/students/:studentName', function(req, res) {
-    console.log(req.params.studentName)
-    /// go to database and search for studentName student
-    // store the data found in this variable - studentDetails
-    //res.send({data: studentDetails})
-    res.send('student data')
 })
-
 
 module.exports = router;
